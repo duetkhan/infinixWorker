b115f1007fe521b04a2c06ab93df7d19740d70a9
duetkhan <bulbulahmedriyan24@gmail.com>
2026-01-23 19:38:08 +0600

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,12 +1,8 @@
-// SMARTLINK
 const SMARTLINK1 = "https://www.effectivegatecpm.com/mpae0kp3zc?key=554362a30b51a44282b343d46a59cd83";
 const SMARTLINK2 = "https://www.effectivegatecpm.com/svxajpx9r?key=5efebfef466ae261927234ad76166944";
 
-/* ============ BANNER 320x50 ============ */
-function showBanner(containerId){
-    const container = document.getElementById(containerId);
-    container.innerHTML = "";
-
+/* Banner Loader */
+function loadBanner(container){
     window.atOptions = {
         'key' : 'b529261b98306ae1169a6bf5b991c3d4',
         'format' : 'iframe',
@@ -15,29 +11,28 @@ function showBanner(containerId){
         'params' : {}
     };
 
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://www.highperformanceformat.com/b529261b98306ae1169a6bf5b991c3d4/invoke.js";
-    container.appendChild(script);
+    const s = document.createElement("script");
+    s.src = "https://www.highperformanceformat.com/b529261b98306ae1169a6bf5b991c3d4/invoke.js";
+    s.type = "text/javascript";
+    container.appendChild(s);
 }
 
-/* ============ POPUNDER ============ */
+/* Popunder */
 function showPopunderAd(){
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://pl28546806.effectivegatecpm.com/b6/e2/25/b6e225daafa502cbf12bb14fdb9532f3.js";
-    document.body.appendChild(script);
+    const s = document.createElement("script");
+    s.src = "https://pl28546806.effectivegatecpm.com/b6/e2/25/b6e225daafa502cbf12bb14fdb9532f3.js";
+    document.body.appendChild(s);
 }
 
-/* ============ NATIVE BANNER ============ */
+/* Native */
 function showNativeAd(){
-    const container = document.createElement("div");
-    container.id = "container-80507bc8e3d998279cd413247931169e";
-    document.body.appendChild(container);
+    const div = document.createElement("div");
+    div.id="container-80507bc8e3d998279cd413247931169e";
+    document.body.appendChild(div);
 
-    const script = document.createElement("script");
-    script.async = true;
-    script.setAttribute("data-cfasync","false");
-    script.src = "https://pl28546803.effectivegatecpm.com/80507bc8e3d998279cd413247931169e/invoke.js";
-    document.body.appendChild(script);
+    const s = document.createElement("script");
+    s.async=true;
+    s.setAttribute("data-cfasync","false");
+    s.src="https://pl28546803.effectivegatecpm.com/80507bc8e3d998279cd413247931169e/invoke.js";
+    document.body.appendChild(s);
 }
